65e787273cdfb53b77dd2efc77826d90a867b4c7
Andii <andii.magyar@gmail.com>
2021-04-09 18:15:27 -0700

    front end

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended:true}));
 app.use(express.json());
+app.use(express.static('public')); //READS CSS AND JAVA FILES!!
 
 
 function filterByQuery(query, animalsArray) {
@@ -69,10 +70,6 @@ function validateAnimal(animal) {
     return true;
 }
 
-app.get('/',(req,res)=>{
-    res.send('home route');
-    })
-
 app.get('/api/animals', (req,res) => {
     let results = animals;
     if (req.query) {
@@ -84,11 +81,11 @@ app.get('/api/animals', (req,res) => {
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
-        res.json(result);
+      res.json(result);
     } else {
-        res.send(404);
+      res.send(404);
     }
-    });
+  });
 
 app.post('/api/animals', (req, res) => {
     req.body.id = animals.length.toString();
@@ -100,6 +97,22 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
       });
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get('*', (req,res) => {
+res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
